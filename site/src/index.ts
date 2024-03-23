eda6b4fd6315a6c255b225f2346b0b5748b3aa76
Eric Gentry <ericsnekbytes@gmail.com>
2024-03-23 18:10:02 -0400

    Refactoring.

--- site/src/index.ts
+++ site/src/index.ts
@@ -1,31 +1,19 @@
 // Main unicode site logic
 
 class UnicodeSite {
-
-  rootNode: any;
   centerColumn: any;
   codepointSearchBox: any;
 
-  constructor(userNode: any) {
+  constructor() {
+    this.centerColumn = document.getElementById('uni_center_column');
 
-    this.rootNode = userNode;
-    this.rootNode.classList.add('uni_root');
-
-    this.centerColumn = document.createElement('div');
-    this.centerColumn.classList.add('uni_center_column');
-    this.rootNode.appendChild(this.centerColumn);
-
-    this.codepointSearchBox = document.createElement('input');
-    this.codepointSearchBox.appearance = 'none';
-    this.codepointSearchBox.classList.add('uni_codepoint_search_box');
-    this.codepointSearchBox.classList.add('root_font');
+    this.codepointSearchBox = document.getElementById('uni_codepoint_search_box');
     this.codepointSearchBox.placeholder = 'Search for a codepoint or name';
-    this.centerColumn.appendChild(this.codepointSearchBox);
   }
 }
 
 window.addEventListener('load', () => {
   console.log('Loading Unicode site!');
-  var site = new UnicodeSite(document.getElementsByTagName('body')[0]);
+  var site = new UnicodeSite();
   (window as any).uniSite = site;
 })
